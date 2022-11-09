fd0135ce4c545610600924d0a875292c009593c2
AgnesM02 <62841666+AgnesM02@users.noreply.github.com>
2022-11-09 17:31:53 +0800

    Add files via upload

---
+++ BTS-Booking Cafe/BTS-Booking Cafe/BTS_Booking_CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTS_Booking_CafeApp.swift
+//  BTS-Booking Cafe
+//
+//  Created by User11 on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct BTS_Booking_CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
